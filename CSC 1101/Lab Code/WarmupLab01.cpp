df9632e41b2f852450c34c4d4c807d96e68365c6
tsquared17 <tsquared17@hotmail.com>
2019-01-09 14:40:40 -0500

    Moved in files

---
+++ CSC 1101/Lab Code/WarmupLab01.cpp
@@ -0,0 +1,48 @@
+//==========================================================
+//
+// Title:      Warm Up Lab 1
+// Course:     CSC 1101
+// Lab Number: Warm Up Lab 1
+// Author:     Trevor Trusty
+// Date:       1/8/2019
+// Description:
+//   This C++ program is the code template for CSC 1101. Every program created in CSC 1101 starts with this code.
+// 
+//
+//==========================================================
+#include <conio.h> // For function getch()
+#include <cstdlib> // For several general-purpose functions
+#include <fstream> // For file handling
+#include <iomanip> // For formatted output
+#include <iostream> // For cin, cout, and system
+#include <string> // For string data type
+using namespace std; // So "std::cout" may be abbreviated to "cout"
+
+int main()
+{
+
+  // Declare variables
+  //int i;
+  //double r;
+  //string s;
+  //char c;
+
+  // Show application header
+  cout << "Welcome to my Application!" << endl;
+  cout << "--------------------------" << endl << endl;
+
+  // Read from console
+  //cout << "Enter a number: ";
+  //cin >> i;
+
+  // Write to screen
+  cout << "Hello World!" << endl;
+
+  // Show application close
+  cout << "\nEnd of my Application" << endl << endl;
+
+  // Pause before application window closes
+  cout << "Press any key to exit ..." << endl;
+  _getch();
+
+}
